d048a35dbf1d6552316a68df9b3fbaa997770439
kurasmagnolia <kingmmcleod@outlook.com>
2025-05-19 12:28:55 -0400

    stashing changes

--- frontend/src/pages/search.jsx
+++ frontend/src/pages/search.jsx
@@ -8,21 +8,20 @@ import 'leaflet/dist/leaflet.css';
 import '../styles/index.css';
 import pinIcon from '../images/pinpointing.png';
 import { SearchBar } from '../components/SearchBar';
-import { coords } from '../adapters/cords-adapter'
-import { useEffect, useState } from "react";
+import { coords } from '../adapters/cords-adapter';
+import { useEffect, useState } from 'react';
 
 export default function search() {
-    const [cords, setCords] = useState([]);
-
+  const [cords, setCords] = useState([]);
 
   useEffect(() => {
     const doFetch = async () => {
       const cord = await coords();
-      console.log('coords', cord)
-      setCords(cord)
+      console.log('coords', cord);
+      setCords(cord);
     };
     doFetch();
- },  [])
+  }, []);
 
   const customIcon = new Icon({
     iconUrl: pinIcon,
@@ -34,9 +33,6 @@ export default function search() {
     <>
       <h1>Search Food Banks Here: </h1>
       <SearchBar />
-      <div> SearchResults </div>
-
-      <p>map here</p>
 
       <div>
         <MapContainer center={[40.7128, -74.006]} zoom={13}>
@@ -45,7 +41,7 @@ export default function search() {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
 
-          {markers.map((marker, index) => (
+          {cords.map((marker, index) => (
             <Marker key={index} position={marker.geocode} icon={customIcon}>
               <Popup>{marker.popUp}</Popup>
             </Marker>
@@ -53,35 +49,13 @@ export default function search() {
         </MapContainer>
       </div>
 
-  return <>
-
-    <h1>Search Food Banks Here: </h1>
-    <SearchBar />
-
-    
-    <div>
-      <MapContainer center={[40.7128, -74.0060]} zoom={13}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        {cords.map((marker, index) => (
-          <Marker key={index} position={marker.geocode} icon={customIcon}>
-            <Popup>{marker.popUp}</Popup>
-          </Marker>
-        ))}
-      </MapContainer>
-        
-    </div>
-
-  <div>
-    <h2>Search by borough:</h2>
+      <div>
+        <h2>Search by borough:</h2>
         <p>probably a list here with basic info:</p>
         <ul>time</ul>
         <ul>location</ul>
         <ul>phone number</ul>
-  </div>
-  
-  </>;
-};
+      </div>
+    </>
+  );
+}
